5fbb62dba073198eaac1a7fddc1fc7c085b8ff2a
industry2205 <63897903+industry2205@users.noreply.github.com>
2020-04-18 18:55:47 +0900

    Update and rename datapretreatment_video.py to VideoToImage.py

--- VideoToImage.py
+++ VideoToImage.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
+# MTCNN을 사용하기 위해 facenet_pytorch 설치
+get_ipython().system('pip install ./facenet-pytorch-vggface2/facenet_pytorch-2.2.7-py3-none-any.whl')
 
 
 import os
@@ -15,12 +13,6 @@ import pandas as pd
 from tqdm.notebook import tqdm
 from facenet_pytorch import MTCNN
 
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-print(f'Running on device: {device}')
-
-
-# In[ ]:
-
 
 # 동영상의 경로 및 라벨의 경로 지정
 TRAIN_DIR = 'D:/'
@@ -30,12 +22,6 @@ METADATA_PATH = TRAIN_DIR + 'metadata.json'
 # 동영상 당 생성 Frame 개수 및 사이즈 지정
 SCALE = 0.25
 N_FRAMES = 3
-
-# MTCNN을 사용하기 위해 facenet_pytorch 설치
-get_ipython().system('pip install ./facenet-pytorch-vggface2/facenet_pytorch-2.2.7-py3-none-any.whl')
-
-
-# In[ ]:
 
 
 # FaceExtractor Class 생성
@@ -48,17 +34,17 @@ class FaceExtractor:
     
     def __call__(self, filename, save_dir):
  
-        # Create video reader and find length
+        # Video Frame 개수
         v_cap = cv2.VideoCapture(filename)
         v_len = int(v_cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        # Pick 'n_frames' evenly spaced frames to sample
+        # Frames 지정에 따라 Numpy 구간 설정
         if self.n_frames is None:
             sample = np.arange(0, v_len)
         else:
             sample = np.linspace(0, v_len - 1, self.n_frames).astype(int)
 
-        # Loop through frames
+        # Frames을 순차적으로 불러와 Image로 저장
         for j in range(v_len):
             success = v_cap.grab()
             if j in sample:
@@ -69,18 +55,15 @@ class FaceExtractor:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 frame = Image.fromarray(frame)
                 
-                # Resize frame to desired size
+                # 지정된 Resolution으로 변경
                 if self.resize is not None:
                     frame = frame.resize([int(d * self.resize) for d in frame.size])
-
+                # 지정된 path에 이미지 저장
                 save_path = os.path.join(save_dir, f'{j}.png')
 
                 self.detector([frame], save_path=save_path)
 
         v_cap.release()
-
-
-# In[ ]:
 
 
 # MTCNN 함수를 face_detector에 저장하여 사용
@@ -93,6 +76,7 @@ face_extractor = FaceExtractor(detector=face_detector, n_frames=N_FRAMES, resize
 all_train_videos = glob.glob(os.path.join(TRAIN_DIR, '*.mp4'))
 
 
+# 동영상 폴더를 지정하여 해당 동영상 이름에 맞는 폴더를 생성 후, 이미지 저장
 with torch.no_grad():
     for path in tqdm(all_train_videos):
         file_name = path.split('/')[-1]
@@ -102,6 +86,4 @@ with torch.no_grad():
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
-        # Detect all faces appear in the video and save them.
         face_extractor(path, save_dir)
-
