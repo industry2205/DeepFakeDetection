6c6293194b3ef31f9d634274b3da0d99903c368b
industry2205 <63897903+industry2205@users.noreply.github.com>
2020-04-19 17:38:28 +0900

    Update EfficientNetWithKeras.py

--- EfficientNetWithKeras.py
+++ EfficientNetWithKeras.py
@@ -38,7 +38,7 @@ X = X[s]
 y = y[s]
 
 #---------------------------------------------------------------------------------------------------
-# Data augumentation 사용하지 않고 훈련
+# Data augmentation 사용하지 않고 훈련
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=321)
